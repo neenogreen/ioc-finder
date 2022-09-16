af642e55ea1109d65cc845674259a052fca0eb04
VINCENZO GIANNUZZO <v.giannuzzo@prismaprogetti.it>
2022-09-16 21:55:15 +0200

    fix: remove print

--- ioc_finder/ioc_finder.py
+++ ioc_finder/ioc_finder.py
@@ -27,8 +27,6 @@ def _listify_with_get_position(indicator_list: ParseResults,text:str) -> List:
         tmp_pos=0
         if len(indicator) > 0:
             position = [_.start() for _ in re.finditer(re.escape(indicator[0]), text)]
-            print(text)
-            print(indicator[0])
             if(indicator[0] in to_dedup):
                 continue
             to_dedup.append(indicator[0])
